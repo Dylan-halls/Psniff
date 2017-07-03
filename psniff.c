59566dfe8561e63d780538bb2f7f33bf8a0e1580
Dylan Halls <Dylan.halls123@gmail.com>
2017-07-03 17:36:56 +0100

    first build

---
+++ psniff.c
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <netinet/udp.h>
+#include <net/if_arp.h>
+
+#include "psniff.h"
+#include "banner.h"
+
+/*Define packet types*/
+int packet_num; //accumilator of packet count
+//Ethernet
+struct ether_header *eth_header;
+char src_mac[5];
+char dst_mac[5];
+int ether_proto;
+//IP
+const struct ip_packet *ip;
+int ip_type_service;   
+int ip_length;      
+int ip_offset;    
+int ip_time_to_live;    
+int ip_proto;  
+int ip_csum;
+int ip_headr_length;
+//ICMP
+const struct icmphdr *icmp_p;
+u_int icmp_type;
+//u_int icmp_seq;
+//TCP
+const struct tcphdr *tcp;
+//UDP
+const struct udphdr *udp;
+int udp_run;
+//ARP
+const struct arphdr *arp;
+const struct arp_hdr *arp_h;
+unsigned short int opcode;
+
+void assign_ethernet(const u_char *packet){
+	eth_header = (struct ether_header *) packet;
+	ether_proto = ntohs(eth_header->ether_type);	
+}
+
+void assign_ip(const u_char *packet){
+	ip = (struct ip_packet*)(packet + ethernet_header_length);
+	ip_type_service = ip->ip_tos;
+	ip_length = ntohs(ip->ip_len);
+	ip_offset = ip->ip_off;
+	ip_time_to_live = ip->ip_ttl;
+	ip_proto = ip->ip_p;
+
+	//disable udp packets
+	if (udp_run != 1){
+		if (ip_proto == IPPROTO_UDP){
+			ip_proto = 99;
+		}
+		else{
+			ip_csum = ntohs(ip->ip_sum);
+			printf("%d ", packet_num);
+			printf("\033[1;32mIPv4 \033[00m \033[1;33m%s\033[00m (\033[1;35m%s\033[00m) \033[1;31m→\033[00m ", getname(inet_ntoa(ip->ip_src)), inet_ntoa(ip->ip_src));
+			printf("\033[1;33m%s\033[00m (\033[1;35m%s\033[00m) ", getname(inet_ntoa(ip->ip_dst)), inet_ntoa(ip->ip_dst));
+			ip_headr_length = getipheader_len(packet);
+		}
+	}
+	else {
+		ip_csum = ntohs(ip->ip_sum);
+		printf("%d ", packet_num);
+		printf("\033[1;32mIPv4 \033[00m \033[1;33m%s\033[00m (\033[1;35m%s\033[00m) \033[1;31m→\033[00m ", getname(inet_ntoa(ip->ip_src)), inet_ntoa(ip->ip_src));
+		printf("\033[1;33m%s\033[00m (\033[1;35m%s\033[00m) ", getname(inet_ntoa(ip->ip_dst)), inet_ntoa(ip->ip_dst));
+		ip_headr_length = getipheader_len(packet);
+	}
+}
+
+void assign_icmp(const u_char *packet){
+	//add id and seq
+	printf("\033[1;32mICMP \033[00m");
+	icmp_p = (struct icmphdr*)(packet + ip_headr_length + ethernet_header_length);
+	icmp_type = (unsigned int)(icmp_p->type);
+	//icmp_seq = (unsigned int) (icmp_p->);
+	printf("%s\n", geticmptype(icmp_type));
+}
+
+void assign_tcp(const u_char *packet){
+	tcp = (struct tcphdr*)(packet + ip_headr_length + ethernet_header_length);
+	printf("\033[1;32mTCP \033[00m");
+	//\033[1;32mARP \033[00m
+	printf("\033[1;30msport=\033[00m\033[0;33m%u\033[00m ", (unsigned int)ntohs(tcp->th_sport));
+	printf("\033[1;30mdport=\033[00m\033[0;33m%u\033[00m ", (unsigned int)ntohs(tcp->th_dport));
+	printf("\033[1;30mseq=\033[00m\033[0;33m%u\033[00m ", (unsigned int)ntohl(tcp->th_seq));
+	printf("\033[1;30mack=\033[00m\033[0;33m%u\033[00m ", (unsigned int)ntohl(tcp->th_ack));
+	printf("\033[1;30mflag=\033[00m\033[0;33m%s\033[00m\n", gettcpflags(tcp));
+}
+
+void assign_udp(const u_char *packet){
+	udp = (struct udphdr*)(packet + ip_headr_length + ethernet_header_length);
+	printf("\033[1;32mUDP \033[00m");
+	printf("\033[1;30msport=\033[00m\033[0;33m%u\033[00m ", (unsigned int)ntohs(udp->uh_sport));
+	printf("\033[1;30mdport=\033[00m\033[0;33m%u\033[00m \n", (unsigned int)ntohs(udp->uh_dport));
+}
+
+void assign_arp(const u_char *packet){
+	arp = (struct arphdr*)(packet + ethernet_header_length);
+	arp_h = (struct arp_hdr*)(packet + ethernet_header_length);
+	printf("%d ", packet_num);
+	printf("\033[1;32mARP \033[00m");
+	opcode = (unsigned short int) ntohs(arp->ar_op);
+	printf("(%s) → ", getarptype(opcode));
+	//Messy but works aha
+	if (opcode == ARPOP_REQUEST){
+		printf("Who has ");
+		makeaddr_d(arp_h);
+		printf("? Tell ");
+		makeaddr_s(arp_h);
+		printf("\n");
+	}
+	if (opcode == ARPOP_REPLY){
+		makeaddr_s(arp_h);
+		printf(" is at ");
+		makemac_s(arp_h);
+		printf("\n");
+	}
+}
+
+void packet_handler(u_char *args, const struct pcap_pkthdr* header, const u_char* packet) {
+	eth_header = (struct ether_header *) packet;
+	assign_ethernet(packet);
+	++packet_num;
+	if (ether_proto == ETHERTYPE_IP){
+		assign_ip(packet);
+		if (ip_proto == IPPROTO_ICMP) {
+			assign_icmp(packet);
+		}
+		else if (ip_proto == IPPROTO_TCP){
+			assign_tcp(packet);
+		}
+		else if (ip_proto == IPPROTO_UDP){
+			assign_udp(packet);
+		}
+	}
+	else if (ether_proto == ETHERTYPE_ARP){
+		assign_arp(packet);
+	}
+}
+
+int main(int argc, char const *argv[])
+{	
+	char *device;
+	int filt;
+	pcap_t *handle;
+	char* bpf_filter = NULL;
+	size_t nbytes = 10;
+	bpf_u_int32 pcap_device_mask;
+	bpf_u_int32 pcap_device_ip;
+	struct bpf_program filter;
+	const char* version = "1.0";
+	unsigned int ts = 0;	
+	char error_buffer[PCAP_ERRBUF_SIZE];
+	void *ptr;
+
+	//TODO: Change these to run from cmd args
+	int snapshot_len = 1028;
+	int promiscuous = 1;
+	int timeout = 1000;
+
+	/* Find a device */
+	device = pcap_lookupdev(error_buffer);
+	if (device == NULL) {
+		fatal("finding a device");
+		return 1;
+	}
+
+	if (pcap_lookupnet(device, &pcap_device_ip, &pcap_device_mask, error_buffer) == -1) {
+		printf("Couldn't get netmask for device %s: %s\n", device, error_buffer);
+		pcap_device_ip = 0;
+		pcap_device_mask = 0;
+	}
+
+	/* handle cmd args */
+	for (int i = 0; i < argc; ++i){
+		if (strncmp(argv[i], "-v", 2) == 0){
+			printf("psniff version %s\n", version);
+			exit(1);
+		}
+		if (strncmp(argv[i], "--filter", 8) == 0){
+			filt = 1;
+		}
+		if (strncmp(argv[i], "--udp", 5) == 0){
+			udp_run = 1;
+		}
+	}
+
+	struct in_addr address;	
+
+    char ip[13];
+    char subnet_mask[13];
+
+    /* Get ip in human readable form */	
+    address.s_addr = pcap_device_ip;
+    strcpy(ip, inet_ntoa(address));
+    if (ip == NULL) {
+        perror("inet_ntoa"); /* print error */
+        return 1;
+    }
+    
+    /* Get subnet mask in human readable form */
+    address.s_addr = pcap_device_mask;
+    strcpy(subnet_mask, inet_ntoa(address));
+    if (subnet_mask == NULL) {
+        perror("inet_ntoa");
+        return 1;
+    }
+
+    banner();
+    //\033[1;3m%s\033[00m
+    printf("\033[1;3m%s\033[00m | ", device);
+    getlocalip();
+    printf("| \033[1;3m%s\033[00m\n\n", subnet_mask);
+
+	handle = pcap_open_live(device, snapshot_len, promiscuous, timeout, error_buffer);
+	if (filt == 1){
+		printf("Filter: \033[1;36m");
+		getline(&bpf_filter, &nbytes, stdin);
+		printf("\033[00m");
+		if (pcap_compile(handle, &filter, bpf_filter, 0, pcap_device_ip) == -1) {
+			printf("\033[1;31mSyntax Error:\033[00m %s", bpf_filter);
+			return 2;
+		}
+		if (pcap_setfilter(handle, &filter) == -1) {
+			printf("Error setting filter - %s\n", pcap_geterr(handle));
+			return 2;
+    	}
+	}
+
+	while(1){
+		pcap_loop(handle, 1, packet_handler, NULL);
+	}
+
+	return 0;
+}
---
+++ psniff.h
@@ -0,0 +1,199 @@
+const u_char *ip_header;
+const u_char *tcp_header;
+const u_char *payload;
+const u_char *arp_header;
+
+int ethernet_header_length = 14;
+int ip_header_length;
+int tcp_header_length;
+int payload_length;
+int arp_length;
+
+struct ip_packet {
+	u_char ip_vhl;
+	u_char ip_tos;   
+	u_short ip_len;   
+	u_short ip_id;     
+	u_short ip_off;    
+	#define IP_RF 0x8000       
+	#define IP_DF 0x4000     
+	#define IP_MF 0x2000  
+	#define IP_OFFMASK 0x1fff 
+	u_char ip_ttl;    
+	u_char ip_p;  
+	u_short ip_sum; 
+	struct in_addr ip_src;
+	struct in_addr ip_dst;
+	};
+
+struct arp_hdr{
+	u_int16_t HardwareType;				// hardware type 
+	u_int16_t ProtocolType;				// protocol type
+
+	u_char HardwareAddressLength;			// harware address length
+	u_char ProtocolAddressLength;			// protocol address length
+	u_int16_t Opcode;				// opcode - request, reply, re request
+
+	u_char s_hw[6];		// source MAC address
+	u_char s_ip[4];		// source IP address
+	u_char d_hw[6];		// target MAC address
+	u_char d_ip[4];
+};
+
+void debug(const char* text) {
+	printf("[DEBUG] %s\n", text);
+}
+
+void fatal(const char *text) {
+	printf("[FATAL] %s\n", text);
+	exit(-1);
+}
+
+const char* getname(const char *ip) 
+{
+	struct in_addr addr;
+	struct hostent *he;
+
+	inet_aton(ip, &addr);
+	if (he = gethostbyaddr(&addr, sizeof(addr), AF_INET)) {
+		return he->h_name;
+	}
+	else {
+		return ip;
+	}
+}
+
+long elapsedtime(){
+	struct timeval tempo1, tempo2;
+	long elapsed_mtime;
+	long elapsed_seconds;
+	long elapsed_useconds;
+	gettimeofday(&tempo2, NULL);
+	elapsed_seconds = tempo2.tv_sec - tempo1.tv_sec; 
+	elapsed_useconds = tempo2.tv_usec - tempo1.tv_usec;
+	return ((elapsed_seconds) * 1000 + elapsed_useconds/1000.0) + 0.5;
+}
+
+int getipheader_len(const u_char *packet){
+	ip_header = packet + ethernet_header_length;
+	ip_header_length = ((*ip_header) & 0x0F);
+	ip_header_length = ip_header_length * 4;
+	return ip_header_length;
+}
+
+const char* geticmptype(u_int type){
+	if (type == ICMP_ECHOREPLY){
+		return "Echo Reply (ping)";
+	}
+	if (type == ICMP_ECHO){
+		return "Echo Request (ping)";
+	}
+	if (type == ICMP_DEST_UNREACH){
+		return "Destination Unreachable (ping)";
+	}
+	if (type == ICMP_HOST_UNKNOWN){
+		return "Host Unknown (ping)";
+	}
+}
+
+const char* gettcpflags(const struct tcphdr *tcp){
+	if(tcp->th_flags & TH_SYN){
+		return "syn";
+	}
+	if(tcp->th_flags & TH_FIN){
+		return "fin";
+	}
+	if(tcp->th_flags & TH_ACK){
+		return "ack";
+	}
+	if(tcp->th_flags & TH_RST){
+		return "rst";
+	}
+	if(tcp->th_flags & TH_PUSH){
+		return "push";
+	}
+	if(tcp->th_flags & TH_URG){
+		return "urg";
+	}
+	if(tcp->th_flags & TH_SYN+TH_ACK){
+		return "syn/ack";
+	}
+}
+
+const char * getarptype(unsigned short int opcode){
+	//TODO: Add suport for other types
+	if (opcode == ARPOP_REQUEST){
+		return "Request";
+	}
+	if (opcode == ARPOP_REPLY){
+		return "Reply";
+	}
+
+}
+//vlabs.c@gmail.com
+void makeaddr_s(const struct arp_hdr *arp_hdr){
+	int i;
+	for(i = 0; i < 4; i++) {	
+		printf("\033[1;35m%d\033[00m", arp_hdr->s_ip[i]);
+		if(i != 3){	
+			printf(".");	
+		}
+	}
+}
+
+void makeaddr_d(const struct arp_hdr *arp_hdr){
+	int i;
+	for(i = 0; i < 4; i++) {	
+		printf("\033[1;35m%d\033[00m", arp_hdr->d_ip[i]);
+		if(i != 3){	
+			printf(".");	
+		}
+	}
+}
+
+void makemac_s(const struct arp_hdr *arp_hdr){
+	int j;
+	for(j = 0; j < 6; j++){
+		printf("\033[1;36m%02x\033[00m", arp_hdr->s_hw[j]);
+		if(j != 5){	
+			printf("\033[1;36m:\033[00m");	
+		}
+	}
+}
+
+void makemac_d(const struct arp_hdr *arp_hdr){
+	int j;
+	for(j = 0; j < 6; j++){	
+		printf("\033[1;36m%02x\033[00m", arp_hdr->d_hw[j]);
+		if(j != 5){	
+			printf("\033[1;36m:\033[00m");	
+		}
+	}
+}
+
+char getfilter(){
+    char file[100];
+    printf("Filter: \n");
+    fgets(file, 100, stdin);
+    return *file;
+}
+
+const char* getlocalip(){
+	FILE *fp;
+	char path[1035];
+
+	/* Open the command for reading. */
+	fp = popen("ifconfig |  grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1' | while read line; do echo -n \"$line \"; done", "r");
+	if (fp == NULL) {
+		printf("Failed to run command\n" );
+		exit(1);
+	}
+
+	/* Read the output a line at a time - output it. */
+	while (fgets(path, sizeof(path)-1, fp) != NULL) {
+		printf("\033[1;3m%s\033[00m", path);
+	}
+
+	/* close */
+	pclose(fp);
+}
